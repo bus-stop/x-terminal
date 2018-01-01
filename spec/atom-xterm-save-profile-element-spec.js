49767efefc25af77b9af9656a9f3586ee5387ebd
Andres Mejia <amejia004@gmail.com>
2017-12-31 19:17:26 -0500

    Add test for save profile modal.

--- spec/atom-xterm-save-profile-element-spec.js
+++ spec/atom-xterm-save-profile-element-spec.js
@@ -16,3 +16,28 @@
  * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+
+import AtomXtermSaveProfileElement from '../lib/atom-xterm-save-profile-element';
+
+describe('AtomXtermSaveProfileElement', () => {
+    this.model;
+
+    beforeEach(() => {
+        this.model = jasmine.createSpyObj('model', ['setElement']);
+    });
+
+    it('initialize()', () => {
+        let element = new AtomXtermSaveProfileElement;
+        element.initialize(this.model);
+        expect(element.messageDiv.textContent).toBe('Enter new profile name');
+    });
+
+    it('setNewTextbox()', () => {
+        let element = new AtomXtermSaveProfileElement;
+        element.initialize(this.model);
+        let textbox = jasmine.createSpyObj('textbox', ['getElement']);
+        textbox.getElement.and.returnValue(document.createElement('div'));
+        element.setNewTextbox(textbox);
+        expect(textbox.getElement).toHaveBeenCalled();
+    });
+});
