4fcde2df94f9550db7e8ada1ef86ffe7ff93ed2d
the-j0k3r <31389848+the-j0k3r@users.noreply.github.com>
2020-03-05 10:17:09 +0000

    chore(scripts): add lintfix + rules

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,6 +2,8 @@ module.exports = {
 	extends: 'stylelint-config-standard',
 	rules: {
 		'declaration-empty-line-before': null,
+		'selector-pseudo-element-colon-notation': 'double',
+		'no-extra-semicolons': true,
 		indentation: 'tab',
 		'selector-type-no-unknown': [
 			true, {
