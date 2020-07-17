a821f6ff8e3683cd93c29b116d0f1474c34800a9
aminya <aminyahyaabadi74@gmail.com>
2020-07-17 03:28:53 -0500

    feat: use babel-preset-atomic

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+const presets = [
+	'babel-preset-atomic',
+]
+
+const plugins = []
+
+module.exports = {
+	presets: presets,
+	plugins: plugins,
+	exclude: 'node_modules/**',
+	sourceMap: 'inline',
+}
