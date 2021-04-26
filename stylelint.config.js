b6e7b719c4e19333885c014496a7f6bf02c81ab3
Tony Brix <tony@brix.ninja>
2021-04-26 09:46:24 -0500

    chore(lint): disable no-invalid-position-at-import-rule

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,6 +2,7 @@ module.exports = {
 	extends: 'stylelint-config-standard',
 	rules: {
 		'declaration-empty-line-before': null,
+		'no-invalid-position-at-import-rule': null,
 		'selector-pseudo-element-colon-notation': 'double',
 		'no-extra-semicolons': true,
 		indentation: 'tab',
