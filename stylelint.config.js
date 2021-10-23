a87c46df9a410799d9885993c70ff30d7a97a0ce
Tony Brix <tony@brix.ninja>
2021-10-22 21:40:41 -0500

    chore: fix lint

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,7 +1,9 @@
 module.exports = {
 	extends: 'stylelint-config-standard',
+	customSyntax: 'postcss-less',
 	rules: {
 		'declaration-empty-line-before': null,
+		'declaration-block-no-redundant-longhand-properties': null,
 		'no-invalid-position-at-import-rule': null,
 		'selector-pseudo-element-colon-notation': 'double',
 		'no-extra-semicolons': true,
